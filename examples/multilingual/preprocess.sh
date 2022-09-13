2b26f945b0f5cdae9eedc82f527f0a0ce419ee65
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-13 04:04:42 -0500

    Update preprocess.sh

--- examples/multilingual/preprocess.sh
+++ examples/multilingual/preprocess.sh
@@ -1,12 +1,12 @@
-SRC=my
-TGT=en
-DATA=$1
-TRAIN=train.alt
-VALID=dev.alt
-TEST=test.alt
+SRC=$1
+TGT=$2
+DATA=data_scripts/data/indic_languages_corpus/indic_languages_corpus/bilingual/$1-$2
+TRAIN=train
+VALID=dev
+TEST=test
 DICT=mbart.cc25.v2/dict.txt
 DEST=data 
-NAME=my_en
+NAME=$1_$2
 fairseq-preprocess \
     --source-lang ${SRC} \
     --target-lang ${TGT} \
