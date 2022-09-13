da780ecf4e001a9851dcef628c27404ec9923e98
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-13 02:50:00 -0500

    Create install_apex.sh

---
+++ examples/multilingual/install_apex.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/NVIDIA/apex
+cd apex
+pip install -v --no-cache-dir --global-option="--cpp_ext" --global-option="--cuda_ext" \
+  --global-option="--deprecated_fused_adam" --global-option="--xentropy" \
+  --global-option="--fast_multihead_attn" ./
