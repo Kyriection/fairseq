12ba74b014dc1e0bfb245bb4c1ffb6f278037289
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-14 22:06:32 -0500

    Create download.sh

---
+++ examples/multilingual/download.sh
@@ -0,0 +1,13 @@
+key=$1-$2
+wget https://data.statmt.org/opus-100-corpus/v1.0/supervised/${key}/opus.${key}-dev.$1
+wget https://data.statmt.org/opus-100-corpus/v1.0/supervised/${key}/opus.${key}-dev.$2
+wget https://data.statmt.org/opus-100-corpus/v1.0/supervised/${key}/opus.${key}-test.$1
+wget https://data.statmt.org/opus-100-corpus/v1.0/supervised/${key}/opus.${key}-test.$2
+wget https://data.statmt.org/opus-100-corpus/v1.0/supervised/${key}/opus.${key}-train.$1
+wget https://data.statmt.org/opus-100-corpus/v1.0/supervised/${key}/opus.${key}-train.$2
+
+
+
+
+
+
