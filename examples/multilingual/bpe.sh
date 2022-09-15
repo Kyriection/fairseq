cd739780229b7239ff7258dc8a42678c5ce24e8a
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-14 21:00:36 -0500

    1

--- examples/multilingual/bpe.sh
+++ examples/multilingual/bpe.sh
@@ -1,18 +1,11 @@
 SPM=../../scripts/spm_encode.py
 MODEL=mbart.cc25.v2/sentence.bpe.model
-DATA=data_scripts/data/indic_languages_corpus/indic_languages_corpus/bilingual/$1-$2
-TRAIN=train
-VALID=dev
-TEST=test
+DATA=data_scripts/data/ML50/raw
+TRAIN=train.$1-$2
+VALID=valid.$1-$2
+TEST=test.$1-$2
 SRC=$1
 TGT=$2
-
-gunzip ${DATA}/${TRAIN}.${SRC}.gz
-gunzip ${DATA}/${TRAIN}.${TGT}.gz
-gunzip ${DATA}/${VALID}.${SRC}.gz
-gunzip ${DATA}/${VALID}.${TGT}.gz
-gunzip ${DATA}/${TEST}.${SRC}.gz
-gunzip ${DATA}/${TEST}.${TGT}.gz
 
 python ${SPM} --model=${MODEL} < ${DATA}/${TRAIN}.${SRC} > ${DATA}/${TRAIN}.spm.${SRC} &
 python ${SPM} --model=${MODEL} < ${DATA}/${TRAIN}.${TGT} > ${DATA}/${TRAIN}.spm.${TGT} &
