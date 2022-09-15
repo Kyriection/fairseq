7aca1f35d1d17e55e3e7a000e396e8fe37ff14d9
Allen <39230030+ZhenyuZhangUSTC@users.noreply.github.com>
2022-09-15 00:27:38 -0500

    Update finetune_multilingual_model.sh

--- examples/multilingual/finetune_multilingual_model.sh
+++ examples/multilingual/finetune_multilingual_model.sh
@@ -16,8 +16,7 @@ fairseq-train "$path_2_data" \
   --encoder-normalize-before --decoder-normalize-before \
   --arch mbart_large --layernorm-embedding \
   --task translation_multi_simple_epoch \
-  --restore-file "$pretrained_model" \
-  --reset-optimizer \
+  --finetune-from-model "$pretrained_model" \
   --sampling-method "temperature" \
   --sampling-temperature "1.5" \
   --encoder-langtok "src" \
@@ -30,5 +29,5 @@ fairseq-train "$path_2_data" \
   --dropout 0.3 --attention-dropout 0.1 --weight-decay 0.0 \
   --max-tokens 1024 --update-freq 2 \
   --save-interval 1 --save-interval-updates 5000 --keep-interval-updates 10 --no-epoch-checkpoints \
-  --seed 222 --log-format simple --log-interval 20 --imp-iters 10 --prune-rate 0.2
+  --seed 222 --log-format simple --log-interval 20
 
